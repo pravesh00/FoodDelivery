9db00b7e6a9a2fce4dc1d6b22079701569d0f2ab
pravesh <pravesh_c@me.iitr.ac.in>
2022-01-30 23:15:18 +0530

    Local Storage Added

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,9 +1,40 @@
-import { createContext,useState } from "react";
+import { createContext,useEffect,useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const FavouritesContext=createContext();
 
 export const FavouritesContextProvider=({children})=>{
     const [favourites, setFavourites]= useState([]);
+
+    const saveFavourites =async (val)=>{
+        try{
+            const jsonValue=JSON.stringify(val);
+            await AsyncStorage.setItem("@favourite",jsonValue);
+
+        }catch(e){
+            console.log(e);
+        }
+    }
+
+    const loadFavourites= async()=>{
+        try{
+            const value=await AsyncStorage.getItem("@favourite");
+            if(val!==null){
+                setFavourites(JSON.parse(value));
+            }
+        }catch(e){
+            console.log(e);
+        }
+    }
+
+    useEffect(()=>{
+        saveFavourites(favourites);
+    },[favourites]);
+
+    useEffect(()=>{
+        loadFavourites();
+    },[]);
+
     const add=(item)=>{
         setFavourites([...favourites,item]);
     }
